b580317ced719a7de9b148174cadb89faf7d9997
Ardiansyah Putra <putraardiansyah530@gmail.com>
2025-01-16 12:42:49 +0700

    Update: Hitung

--- resources/js/Pages/Hitung.jsx
+++ resources/js/Pages/Hitung.jsx
@@ -18,124 +18,235 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import { Separator } from '@/components/ui/separator';
+import { useForm } from '@inertiajs/react';
 import { Calculator, CirclePlus, Play } from 'lucide-react';
+import { useState } from 'react';
+
+// Komponen Form Kriteria
+function KriteriaForm({ kriteria, setKriteria }) {
+    const handleKriteriaChange = (index, field, value) => {
+        const updatedKriteria = [...kriteria];
+        updatedKriteria[index][field] = value;
+        setKriteria(updatedKriteria);
+    };
+
+    const handleAddKriteria = () => {
+        setKriteria([...kriteria, { nama_kriteria: "", bobot: 0, jenis: "benefit" }]);
+    };
+
+    return (
+        <Card className={'shadow-lg'}>
+            <CardHeader>
+                <CardTitle>Kriteria</CardTitle>
+                <CardDescription>Silahkan input kriteria</CardDescription>
+            </CardHeader>
+            <CardContent>
+                {kriteria.map((k, index) => (
+                    <div key={index} className="grid grid-cols-3 w-full items-center gap-4 mb-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor={`kriteria-${index}`}>Kriteria</Label>
+                            <Input
+                                id={`kriteria-${index}`}
+                                placeholder="Kriteria"
+                                value={k.nama_kriteria}
+                                onChange={(e) => handleKriteriaChange(index, 'nama_kriteria', e.target.value)}
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor={`bobot-${index}`}>Bobot</Label>
+                            <Input
+                                id={`bobot-${index}`}
+                                placeholder="Bobot"
+                                type="number"
+                                value={k.bobot}
+                                onChange={(e) => handleKriteriaChange(index, 'bobot', parseFloat(e.target.value))}
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor={`jenis-${index}`}>Cost/Benefit</Label>
+                            <Select onValueChange={(value) => handleKriteriaChange(index, 'jenis', value)}>
+                                <SelectTrigger id={`jenis-${index}`}>
+                                    <SelectValue placeholder="Cost/Benefit" value={k.jenis} />
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+                                    <SelectItem value="cost">Cost</SelectItem>
+                                    <SelectItem value="benefit">Benefit</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                ))}
+            </CardContent>
+            <CardFooter className="flex justify-end">
+                <Button onClick={handleAddKriteria}>
+                    <CirclePlus />Tambah Kriteria
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}
+
+// Komponen Form Data Rumah
+function RumahForm({ kriteria, rumah, setRumah }) {
+    const handleRumahChange = (index, field, value) => {
+        const updatedRumah = [...rumah];
+        updatedRumah[index][field] = value;
+        setRumah(updatedRumah);
+    };
+
+    const handleAddRumah = () => {
+        const newRumah = { nama_rumah: "" };
+        kriteria.forEach((_, i) => {
+            newRumah[`kriteria${i + 1}`] = 0;
+        });
+        setRumah([...rumah, newRumah]);
+    };
+
+    return (
+        <Card className={'shadow-lg'}>
+            <CardHeader>
+                <CardTitle>Data Rumah</CardTitle>
+                <CardDescription>Silahkan input data rumah</CardDescription>
+            </CardHeader>
+            <CardContent>
+                {rumah.map((r, rumahIndex) => (
+                    <div key={rumahIndex} className="grid grid-cols-1 w-full gap-4">
+                        <div className="grid grid-cols-7 items-center gap-4">
+                            <div className="col-span-2 flex flex-col space-y-1.5">
+                                <Label htmlFor={`nama_rumah-${rumahIndex}`}>Nama Rumah</Label>
+                                <Input
+                                    id={`nama_rumah-${rumahIndex}`}
+                                    placeholder="Nama Rumah"
+                                    value={r.nama_rumah}
+                                    onChange={(e) => handleRumahChange(rumahIndex, 'nama_rumah', e.target.value)}
+                                />
+                            </div>
+                            {kriteria.map((k, kriteriaIndex) => (
+                                <div key={kriteriaIndex} className="flex flex-col space-y-1.5">
+                                    <Label htmlFor={`kriteria-${kriteriaIndex + 1}-${rumahIndex}`}>{k.nama_kriteria || `Kriteria ${kriteriaIndex + 1}`}</Label>
+                                    <Input
+                                        id={`kriteria-${kriteriaIndex + 1}-${rumahIndex}`}
+                                        placeholder={`Kriteria ${kriteriaIndex + 1}`}
+                                        type="number"
+                                        value={r[`kriteria${kriteriaIndex + 1}`]}
+                                        onChange={(e) => handleRumahChange(rumahIndex, `kriteria${kriteriaIndex + 1}`, parseFloat(e.target.value))}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </CardContent>
+            <CardFooter className="flex justify-end">
+                <Button onClick={handleAddRumah}>
+                    <CirclePlus />Tambah Data Rumah
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+}
+
+// Komponen Hasil Perhitungan
+function Hasil({ results }) {
+    return (
+        <Card className={'shadow-lg'}>
+            <CardHeader>
+                <CardTitle>Hasil Perhitungan</CardTitle>
+                <CardDescription>Hasil perhitungan SPK</CardDescription>
+            </CardHeader>
+            <CardContent>
+                {results && results.length > 0 ? (
+                    <div className="overflow-x-auto">
+                        <table className="w-full text-left">
+                            <thead>
+                                <tr>
+                                    <th className="py-2 px-4 font-semibold border-b">Peringkat</th>
+                                    <th className="py-2 px-4 font-semibold border-b">Nama Rumah</th>
+                                    <th className="py-2 px-4 font-semibold border-b">Score</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {results.map((result, index) => (
+                                    <tr key={index} className={index % 2 === 0 ? "bg-gray-50" : ""}>
+                                        <td className="py-2 px-4 border-b">{index + 1}</td>
+                                        <td className="py-2 px-4 border-b">{result.nama_rumah}</td>
+                                        <td className="py-2 px-4 border-b">{result.score.toFixed(2)}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                ) : (
+                    <p>Tidak ada hasil perhitungan.</p>
+                )}
+            </CardContent>
+        </Card>
+    );
+}
 
 export default function Hitung() {
+    const { data, setData, post, processing, errors } = useForm({
+        kriteria: [],
+        rumah: [],
+        results: [],
+    });
+    const [kriteria, setKriteria] = useState(data.kriteria);
+    const [rumah, setRumah] = useState(data.rumah);
+    const [results, setResults] = useState(data.results);
+
+    const hitung = () => {
+        if (!kriteria.length || !rumah.length) {
+            alert("Kriteria dan data rumah harus diisi!");
+            return;
+        }
+
+        const totalBobot = kriteria.reduce((sum, k) => sum + parseFloat(k.bobot), 0);
+        const normalizedWeights = kriteria.map(k => k.bobot / totalBobot);
+
+        const normalizedData = rumah.map(r => {
+            const normalized = { nama_rumah: r.nama_rumah, score: 0 };
+            kriteria.forEach((k, i) => {
+                const value = parseFloat(r[`kriteria${i + 1}`]);
+                if (k.jenis === "benefit") {
+                    const maxValue = Math.max(...rumah.map(r => parseFloat(r[`kriteria${i + 1}`])));
+                    normalized[`kriteria${i + 1}`] = value / maxValue;
+                } else {
+                    const minValue = Math.min(...rumah.map(r => parseFloat(r[`kriteria${i + 1}`])));
+                    normalized[`kriteria${i + 1}`] = minValue / value;
+                }
+            });
+            return normalized;
+        });
+
+        const finalResults = normalizedData.map((n, ri) => {
+            let score = 0;
+            kriteria.forEach((_, ki) => {
+                score += n[`kriteria${ki + 1}`] * normalizedWeights[ki];
+            });
+            return { nama_rumah: n.nama_rumah, score };
+        });
+
+        finalResults.sort((a, b) => b.score - a.score);
+        setResults(finalResults);
+    };
+
     return (
         <AppLayout title={'Hitung'}>
-            <div className={'h-dvh flex flex-col items-center justify-center relative before:w-96 before:h-96 before:absolute before:rounded-full before:bg-black before:left-[50%] before:top-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:blur-[200px] before:-z-10'}>
-                <Card className={'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-lg'}>
-                    <CardHeader>
-                        <CardTitle>Kriteria</CardTitle>
-                        <CardDescription>silahkan input kriteria</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <form>
-                            <div className="grid grid-cols-3 w-full items-center gap-4">
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="kriteria">Kriteria</Label>
-                                    <Input id="kriteria" placeholder="Kriteria" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="bobot">Bobot</Label>
-                                    <Input id="bobot" placeholder="Bobot" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="costOrBenevit">Cost/Benevit</Label>
-                                    <Select>
-                                        <SelectTrigger id="costOrBenevit">
-                                            <SelectValue placeholder="Cost/Benevit" />
-                                        </SelectTrigger>
-                                        <SelectContent position="popper">
-                                            <SelectItem value="cost">Cost</SelectItem>
-                                            <SelectItem value="benevit">Benevit</SelectItem>
-                                        </SelectContent>
-                                    </Select>
-                                </div>
-                            </div>
-                        </form>
-                    </CardContent>
-                    <CardFooter className="flex justify-between">
-                        <Button>
-                            <CirclePlus />Tambah Kriteria
-                        </Button>
-                        <Button>
-                            <Play />Mulai
-                        </Button>
-                    </CardFooter>
-                </Card>
-            </div>
-            <div className={'h-dvh flex flex-col items-center justify-center relative before:w-96 before:h-96 before:absolute before:rounded-full before:bg-black before:left-[50%] before:top-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:blur-[200px] before:-z-10'}>
-                <Card className={'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-lg'}>
-                    <CardHeader>
-                        <CardTitle>Data</CardTitle>
-                        <CardDescription>silahkan input data</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <form>
-                            <div className="grid grid-cols-7 w-full items-center gap-4">
-                                <div className="col-span-2 flex flex-col space-y-1.5">
-                                    <Label htmlFor="nama_rumah">Nama Rumah</Label>
-                                    <Input id="nama_rumah" placeholder="Nama Rumah" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="kriteria_1">Kriteria 1</Label>
-                                    <Input id="kriteria_1" placeholder="Kriteria 1" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="kriteria_2">Kriteria 2</Label>
-                                    <Input id="kriteria_2" placeholder="Kriteria 2" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="kriteria_3">Kriteria 3</Label>
-                                    <Input id="kriteria_3" placeholder="Kriteria 3" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="kriteria_4">Kriteria 4</Label>
-                                    <Input id="kriteria_4" placeholder="Kriteria 4" />
-                                </div>
-                                <div className="flex flex-col space-y-1.5">
-                                    <Label htmlFor="kriteria_5">Kriteria 5</Label>
-                                    <Input id="kriteria_5" placeholder="Kriteria 5" />
-                                </div>
-                            </div>
-                        </form>
-                    </CardContent>
-                    <CardFooter className="flex justify-between">
-                        <Button>
-                            <CirclePlus />Tambah Data
-                        </Button>
-                        <Button>
-                            <Calculator />Hitung
-                        </Button>
-                    </CardFooter>
-                </Card>
-            </div>
-            <div className={'h-dvh flex flex-col items-center justify-center relative before:w-96 before:h-96 before:absolute before:rounded-full before:bg-black before:left-[50%] before:top-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:blur-[200px] before:-z-10'}>
-                <Card className={'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-lg'}>
-                    <CardHeader>
-                        <CardTitle>Hasil</CardTitle>
-                        <CardDescription>hasil yang di dapat</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <Card className={'p-5'}>
-                            <div className={'grid gap-2 items-center justify-center'}>
-                                <p>Terbaik 1 : ini adalah terbaik ke-1</p>
-                                <Separator />
-                                <p>Terbaik 2 : ini adalah terbaik ke-2</p>
-                                <Separator />
-                                <p>Terbaik 3 : ini adalah terbaik ke-3</p>
-                                <Separator />
-                                <p>Terbaik 4 : ini adalah terbaik ke-4</p>
-                                <Separator />
-                                <p>Terbaik 5 : ini adalah terbaik ke-5</p>
-                                <Separator />
-                                <p>Terbaik 6 : ini adalah terbaik ke-6</p>
-                                <Separator />
-                            </div>
-                        </Card>
-                    </CardContent>
-                </Card>
+            <div className={'p-4 container mx-auto'}>
+                <div className={'mb-8'}>
+                    <KriteriaForm kriteria={kriteria} setKriteria={setKriteria} />
+                </div>
+                <div className={'mb-8'}>
+                    <RumahForm kriteria={kriteria} rumah={rumah} setRumah={setRumah} />
+                </div>
+                <div className={'mb-8 flex justify-center'}>
+                    <Button onClick={hitung}>
+                        <Calculator />Hitung
+                    </Button>
+                </div>
+                <div>
+                    <Hasil results={results} />
+                </div>
             </div>
         </AppLayout>
     );
