eb481c14972c033d1b0b82449224cfc1b019b626
Ardiansyah Putra <putraardiansyah530@gmail.com>
2025-01-17 14:43:20 +0700

    Update: Home

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -8,10 +8,8 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import { Badge } from '@/components/ui/badge';
-import { Separator } from '@/components/ui/separator';
-import { Play } from 'lucide-react';
-
+import { CirclePlay } from 'lucide-react';
+import Background from '@/assets/img/Perumahan.jpg';
 export default function Home() {
     const rules = [
         {
@@ -30,39 +28,26 @@ export default function Home() {
 
     return (
         <AppLayout title={'Home'}>
-            <div className={'h-dvh flex flex-col items-center justify-center gap-5'}>
-                <div className={'text-5xl font-extrabold before:w-[600px] before:h-[600px] before:bg-gradient-to-br before:from-black before:from-50% before:to-black before:to-50% before:absolute before:-z-10 before:rounded-full before:top-[50%] before:left-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:blur-[300px]'}>
-                    <p>Selamat Datang di Website Sistem Penunjang Keputusan</p>
-                    <p>Pembelian Rumah Menggunakan Metode <span className={'text-6xl bg-black/70 text-white rounded-full px-7'}>MOORA</span></p>
+            <img src={Background} className={'absolute top-0 left-0 -z-10 w-full brightness-[.2]'} />
+            <div className={'mx-40 md:mx-0 h-dvh flex flex-col items-center justify-center gap-5'}>
+                <div className={'text-5xl font-extrabold'}>
+                    <p className={'text-white'}>Selamat Datang di Website Perhitungan Rumah di Tangerang Selatan</p>
                 </div>
-                <Badge variant={'secondary'} className={'text-xl'}>Temukan solusi terbaik</Badge>
                 <Button className={'bg-blue-500'} onClick={() => router.get(route('hitung'))}>
-                    <Play />Mulai Hitung
+                    Mulai Hitung
                 </Button>
-                <Separator className={'bg-gray-300'} />
                 <div>
                     <Card>
                         <CardHeader>
-                            <CardTitle>Aturan Perhitungan</CardTitle>
+                            <CardTitle>Video Tutorial</CardTitle>
                             <CardDescription>Silahkan pahami aturan perhitungan</CardDescription>
                         </CardHeader>
-                        <CardContent>
-                            <form>
-                                <div className="grid w-full items-center gap-4">
-                                    <div>
-                                        {rules.map(rule => (
-                                            <div key={rule.rule} className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                                <div className="space-y-1">
-                                                    <p className="text-sm font-medium leading-none">
-                                                        {rule.rule}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
+                        <CardContent className={'flex items-center justify-center h-28'}>
+                            <div>
+                                <div>
+                                    <CirclePlay />
                                 </div>
-                            </form>
+                            </div>
                         </CardContent>
                     </Card>
                 </div>
